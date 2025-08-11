e71db1c91f33b6ed78fd3a63aeab04d50502687b
Influcent Tech <dahiyamayank0@gmail.com>
2025-08-11 12:22:17 +0530

    Rate limiter limits

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,20 +1,18 @@
 /*
-* IMPORTS
-*/
+ * IMPORTS
+ */
 import rateLimit from 'express-rate-limit';
-
 
 /*
  * Global rate limiter to protect from brute force attacks.
  */
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 mins
-    max: 10, // max requests per IP
-    message: { status: 'TOO_MANY_REQUESTS', message: 'Too many requests, try again later' }
+  windowMs: 15 * 60 * 1000, // 15 mins
+  max: 100, // max requests per IP
+  message: { status: 'TOO_MANY_REQUESTS', message: 'Too many requests, try again later' },
 });
 
-
 /*
-* EXPORTS
-*/
+ * EXPORTS
+ */
 export default limiter;
